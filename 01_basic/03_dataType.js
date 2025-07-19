ae36ca0c3912babb0c0f950ecbe78fc1ae6d439a
Pradip Hazra <codedot12@gmail.com>
2025-07-19 18:10:19 +0000

    dataType in javascript

---
+++ 01_basic/03_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)   // we are using nodejs, not browser
+
+console.log(3 + 
+    6);   // code readability should be high
+ console.log("Hello Pradip")
+
+
+let name = "Pradip"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => '' & ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Pradip");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
