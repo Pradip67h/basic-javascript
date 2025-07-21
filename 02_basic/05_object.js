81b7319e49f93cfa9852fe687d9561db66aa1609
Pradip Hazra <codedot12@gmail.com>
2025-07-21 14:38:32 +0000

    json in js

---
+++ 02_basic/05_object.js
@@ -0,0 +1,27 @@
+const course = {
+    coursename: "javascript in hindi",
+    price: "1599",
+    courseInstructor: "Pradip",
+    timeDuration: "6 Month"
+}
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+const navbar = ({company}) => {
+
+}
+navbar(company = "Pradip Academy") 
+console.log(company);
+
+// {
+//     "name":"Pradip",
+//     "coursename": "js in bemgali",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
