a20e51ef1fd7ddbe4896c5ce6887831a9f1c0c63
Pradip Hazra <codedot12@gmail.com>
2025-07-24 15:05:51 +0000

    arrow in javascript

---
+++ 03_basic/05_arrow.js
@@ -0,0 +1,21 @@
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addTwo(3, 4));        // 7
+
+const addThree = (num1, num2, num3) => num1 + num2 + num3
+
+console.log(addThree(4, 8, 9));      // 21
+
+
+const addName = (num1, num2) => ({username: "Pradip"})
+console.log(addName(6, 8));       //  { username: 'Pradip' }
+
+
+// const myArray = [2, 4, 6, 7, 9]
+
+// myArray.forEach()
+
+
+
