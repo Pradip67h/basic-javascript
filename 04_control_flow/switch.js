510ccd382ae5195f3482367aca6ff565a16cc1c3
Pradip Hazra <codedot12@gmail.com>
2025-07-25 11:25:22 +0000

    switch statement in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,77 @@
+//  ++++++++++++++ SWITCH CASE STATEMENT ++++++++++++++
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("Aprile");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
+
+
+const day = "sun";
+
+switch (day) {
+    case "sun":
+        console.log("Sun Day");
+        break;
+    case "mon":
+        console.log("Mon Day");
+        break;
+    case "tue":
+        console.log("Tues Day");
+        break;
+    case "wed":
+        console.log("Wednes Day");
+        break;
+    case "thu":
+        console.log("Thurs Day");
+        break;
+    case "fir":
+        console.log("Fir Day");
+        break;
+    case "sat":
+        console.log("Satur Day");
+        break;
+
+    default:
+        console.log("not any case in match");
+        break;
+}
