34d8dc2cbfc47b9e2c6c1a29cfdd0e5e9e77bfd7
Pradip Hazra <codedot12@gmail.com>
2025-08-03 15:31:42 +0000

    while loop in javascript

---
+++ 05_iterations/while_loop.js
@@ -0,0 +1,17 @@
+// while loop
+
+let index = 0;
+while(index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+
+
+myArray = ["flash", "batman", "superman", "ironman"];
+
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
