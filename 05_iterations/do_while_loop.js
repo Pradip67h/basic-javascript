ad02c68a2678764c8bb8f6b12aa40e398e3558a4
Pradip Hazra <codedot12@gmail.com>
2025-08-03 15:40:38 +0000

    do while loop using javascript

---
+++ 05_iterations/do_while_loop.js
@@ -0,0 +1,21 @@
+// do while loop 
+
+let score = 1;
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
+
+
+// basic 1 to 5 print using do while loop
+let result = "";
+let i = 0;
+do {
+  i += 1;
+  result += `${i} `;
+} while (i > 0 && i < 5);
+
+console.log(result);
+
+
